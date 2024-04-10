c21b68477bd5914c05f13987263e292a9a6ba993
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-10 22:54:21 +0200

    Implemented user IO
    user can specify: csvFilePath, jsonFilePath, which pokemon data to convert

--- PokemonJSONTool/PokemonJSONTool/Program.cs
+++ PokemonJSONTool/PokemonJSONTool/Program.cs
@@ -1,10 +1,103 @@
-﻿namespace PokemonJSONTool
+﻿using PokemonJSONTool.CSVStrategy;
+using PokemonJSONTool.Helper;
+using PokemonJSONTool.JSONStrategy;
+using PokemonJSONTool.PokemonDataStrategy;
+using System.Text.Json.Serialization;
+
+namespace PokemonJSONTool
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("--Pokemon JSON File Tool--");
+                Console.WriteLine("Which JSON do you want to update? : ");
+                Console.WriteLine("Pokemon General Data: 1");
+                Console.WriteLine("Pokemon Moveset Data: 2");
+
+                int option = GetOption();
+                IPokemonDataStategy pokemonDataStategy;
+
+                switch (option)
+                {
+                    case 1:
+                        pokemonDataStategy = new PokemonGeneralDataStrategy();
+                        break;
+                    case 2:
+                        pokemonDataStategy = new PokemonMovesetDataStrategy();
+                        break;
+                    default:
+                        continue;
+                }
+
+                string csvFilePath = string.Empty;
+                int tries = 0;
+                while (tries < 5)
+                {
+                    Console.WriteLine("Specify where the data is saved...");
+                    csvFilePath = Console.ReadLine() ?? string.Empty;
+
+                    if (FileHelper.Exists(csvFilePath))
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("this is not a valid path, try again.");
+                    csvFilePath = string.Empty;
+                    tries++;
+                }
+
+                if(csvFilePath == string.Empty)
+                {
+                    continue;
+                }
+
+                string saveFilePath = string.Empty;
+                Console.WriteLine("Specify where the data is saved...");
+                saveFilePath = Console.ReadLine() ?? string.Empty;
+
+                if (saveFilePath == string.Empty)
+                {
+                    continue;
+                }
+
+                ICSVStrategy csvStrategy = new StreamReaderStrategy();
+                IJSONStrategy jsonStrategy = new NewtonSoftStrategy();
+                pokemonDataStategy.ConvertDataToJson(
+                    csvStrategy,
+                    jsonStrategy,
+                    csvFilePath,
+                    saveFilePath);
+            }
         }
+
+        private static int GetOption()
+        {
+            bool reading = true;
+            while (reading)
+            {
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        return 1;
+                    case "2":
+                        return 2;
+                    default:
+                        continue;
+                }
+            }
+            return 0;
+        }
+
+        private static string GetSaveFilePath()
+        {
+            return string.Empty;
+        }
+
+        
+
     }
 }
