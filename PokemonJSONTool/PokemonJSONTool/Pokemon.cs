6b9771b94d07ea231347380981658bf01e6d157f
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-10 21:03:52 +0200

    Added JSON tool
    Added JSON tool
    Added Pokemon Data Classes to JSON tool

---
+++ PokemonJSONTool/PokemonJSONTool/Pokemon.cs
@@ -0,0 +1,44 @@
+﻿namespace BattleFactoryBlindPracticer.Models
+{
+    public struct Types
+    {
+        Type Type1;
+        Type Type2;
+    }
+
+    public struct Stats
+    {
+        int HP;
+        int Att;
+        int Def;
+        int SpAtt;
+        int SpDef;
+        int Spe;
+    }
+
+    public struct Abilities
+    {
+        Ability Ability1;
+        Ability Ability2;
+    }
+
+
+    public class Pokemon
+    {
+        public Pokemon() { }
+
+        public Pokemon(int pokedexNumber) {
+            this.PokedexNumber = pokedexNumber;
+        }
+
+        public int PokedexNumber { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public Types Typing { get; set; }
+
+        public Stats Stats { get; set; }
+
+        public Abilities Abilities { get; set; }
+    }
+}
---
+++ PokemonJSONTool/PokemonJSONTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonJSONTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
