2797da87fb98503361f4bd4c353a7e8f26ac223c
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-10 22:51:56 +0200

    Added Json Strategy Pattern

---
+++ PokemonJSONTool/PokemonJSONTool/JSONStrategy/IJSONStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PokemonJSONTool.NewFolder
+{
+    public interface IJSONStrategy
+    {
+        public void SaveJSON(string fileName);
+    }
+}
