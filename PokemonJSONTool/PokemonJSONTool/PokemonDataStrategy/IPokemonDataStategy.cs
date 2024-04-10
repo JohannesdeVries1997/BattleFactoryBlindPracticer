b178cc2fdd0bc42323ae7e92c39e0f72c6d531bb
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-10 22:53:11 +0200

    Added PokemonData Strategy pattern

---
+++ PokemonJSONTool/PokemonJSONTool/PokemonDataStrategy/IPokemonDataStategy.cs
@@ -0,0 +1,15 @@
+﻿using PokemonJSONTool.CSVStrategy;
+using PokemonJSONTool.JSONStrategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonJSONTool.PokemonDataStrategy
+{
+    public interface IPokemonDataStategy
+    {
+        public void ConvertDataToJson(ICSVStrategy csvStrategy, IJSONStrategy jSONStrategy,string csvFilePath, string jsonSavePath) { }
+    }
+}
