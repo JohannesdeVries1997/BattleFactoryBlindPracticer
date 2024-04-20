544ce31379ab8df482283d1512cce9010c4cb839
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-20 23:00:38 +0200

    Added RoundSelectorButton

---
+++ BattleFactoryBlindPracticer/BattleFactoryBlindPracticer/Shared/RoundSelectorButton.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BattleFactoryBlindPracticer.Shared
+{
+    public partial class RoundSelectorButton
+    {
+        [Parameter]
+        public int RoundNumber { get; set; } = 0;
+
+        public EventCallback<int> EventCallback { get; set; } = new();
+
+        private string styleString = "unpressed";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        public void OnClick()
+        {
+            EventCallback.InvokeAsync(this.RoundNumber);
+            styleString = "pressed";
+            StateHasChanged();
+        }
+
+        public void Unpress()
+        {
+            styleString = "unpressed";
+            StateHasChanged();
+        }
+    }
+}
