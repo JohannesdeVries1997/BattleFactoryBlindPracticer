e918008083c3b77916979421125ef65cca5f0ae8
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-20 04:18:41 +0200

    Added PokemonGuesserComponent
    Basic layout
    Basic functionality, no reset yet
    Refactored a lot of components
    renamed some functions

---
+++ BattleFactoryBlindPracticer/BattleFactoryBlindPracticer/Model/PokemonExtension.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+
+namespace BattleFactoryBlindPracticer.Model
+{
+    public static class PokemonExtension
+    {
+        public static bool TryGetPokemonByDexNumber(this List<Pokemon> pokemons, int pokedexNumber, out Pokemon pokemon)
+        {
+            foreach (Pokemon p in pokemons)
+            {
+                if (p.PokedexNumber == pokedexNumber)
+                {
+                    pokemon = p;
+                    return true;
+                }
+            }
+            pokemon = new Pokemon();
+            return false;
+        }
+
+        public static bool TryGetPokemonByName(this List<Pokemon> pokemons, string pokemonName, out Pokemon pokemon)
+        {
+            foreach (Pokemon p in pokemons)
+            {
+                if (CompareStrings(p.Name, pokemonName))
+                {
+                    pokemon = p;
+                    return true;
+                }
+            }
+            pokemon = new Pokemon();
+            return false;
+        }
+
+        public static void GetEmptyPokemon(this Pokemon pokemon)
+        {
+            pokemon.PokedexNumber = 0;
+            pokemon.Name = "";
+            pokemon.Type1 = "None";
+            pokemon.Type2 = "None";
+            pokemon.Ability1 = "None";
+            pokemon.Ability2 = "None";
+            pokemon.HP = 0;
+            pokemon.Att = 0;
+            pokemon.Def = 0;
+            pokemon.SpAtt = 0;
+            pokemon.SpDef = 0;
+            pokemon.Spe = 0;
+        }
+
+        private static bool CompareStrings(string a, string b)
+        {
+            string aLower = a.ToLower();
+            string bLower = b.ToLower();
+            return (a == b);
+        }
+    }
+}
