f3ad28b07e62bf3c7c55889605a611da9b7ddfae
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-26 18:38:09 +0200

    Added NatureComponent

---
+++ BattleFactoryBlindPracticer/BattleFactoryBlindPracticer/Shared/NatureComponent.razor.cs
@@ -0,0 +1,36 @@
+﻿using BattleFactoryBlindPracticer.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BattleFactoryBlindPracticer.Shared
+{
+    public partial class NatureComponent
+    {
+        [Parameter]
+        public string Nature { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool IsGuessable { get; set; } = false;
+
+        private GuessComponent guessComponent = new();
+        private List<string> autoCompleteList = new();
+
+        private bool confirmButtonDisabled = false;
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            confirmButtonDisabled = !IsGuessable;
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            autoCompleteList = await DataGetter.GetAllNatures(_client);
+        }
+
+        private void HandleConfirm()
+        {
+            guessComponent.HandleGuessCheck();
+            confirmButtonDisabled = true;
+        }
+    }
+}
