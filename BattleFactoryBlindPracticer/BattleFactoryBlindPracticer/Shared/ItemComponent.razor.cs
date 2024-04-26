9489742fabbf1738b409caa5ae5e31db30c30551
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-27 00:49:40 +0200

    Added ItemComponent

---
+++ BattleFactoryBlindPracticer/BattleFactoryBlindPracticer/Shared/ItemComponent.razor.cs
@@ -0,0 +1,37 @@
+﻿using BattleFactoryBlindPracticer.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BattleFactoryBlindPracticer.Shared
+{
+    public partial class ItemComponent
+    {
+        [Parameter]
+        public string Item { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool IsGuessable { get; set; } = false;
+
+        private GuessComponent guessComponent { get; set; } = new();
+
+        private bool confirmButtonDisabled = false;
+
+        private List<string> autoCompleteList = new();
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            confirmButtonDisabled = !IsGuessable;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            autoCompleteList = await DataGetter.GetOptionList(_client, "Item");
+        }
+
+        private void ConfirmClick()
+        {
+            confirmButtonDisabled = true;
+            guessComponent.HandleGuessCheck();
+        }
+    }
+}
