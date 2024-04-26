9777f4a93fec61241139bc4b69f5dd1deeb1f167
JohannesdeVries1997 <johannes_dv@hotmail.com>
2024-04-27 01:40:39 +0200

    StringComparison now excludes various characters

--- BattleFactoryBlindPracticer/BattleFactoryBlindPracticer/Model/StringHelper.cs
+++ BattleFactoryBlindPracticer/BattleFactoryBlindPracticer/Model/StringHelper.cs
@@ -4,9 +4,24 @@
     {
         public static bool CompareStrings(string a, string b)
         {
-            string aLower = a.ToLower();
-            string bLower = b.ToLower();
+            string aLower = a.ToLower().RemoveSpecialCharacters();
+            string bLower = b.ToLower().RemoveSpecialCharacters();
             return (aLower == bLower);
+        }
+
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            char[] illegalCharacters = {' ','\t','\n','-','.',',','\''};
+            List<char> illegalCharacterList = new(illegalCharacters);
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (illegalCharacterList.Contains(str[i]))
+                {
+                    str = str.Remove(i, 1);
+                }
+            }
+
+            return str;
         }
     }
 }
